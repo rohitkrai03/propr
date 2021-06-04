d840143d23f707ec33679fd0ded0f2241ff7a438
Rohit Rai <rohitkrai03@gmail.com>
2021-06-04 14:30:08 +0530

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search input', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputElement = screen.getAllByPlaceholderText(/search/i);
+  expect(inputElement[0]).toBeInTheDocument();
 });
